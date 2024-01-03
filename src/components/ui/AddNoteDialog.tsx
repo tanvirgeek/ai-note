7553ed0190d0eac0ba29675f622878a5d56e2fe4
tanvirgeek <tanvirgeek@gmail.com>
2024-01-03 06:06:05 +0600

    Creating Notes in DB

--- src/components/ui/AddNoteDialog.tsx
+++ src/components/ui/AddNoteDialog.tsx
@@ -2,7 +2,25 @@ import { CreateNoteSchema, createNoteSchema } from "@/lib/validation/note";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
+import { Input } from "./input";
+import { Textarea } from "./textarea";
+import LoadingButton from "./loading-button";
+import { useRouter } from "next/navigation";
 
 interface AddNoteDialogProps {
   open: boolean;
@@ -10,12 +28,31 @@ interface AddNoteDialogProps {
 }
 
 const AddNoteDialog = ({ open, setOpen }: AddNoteDialogProps) => {
+  const router = useRouter()
+
   const form = useForm<CreateNoteSchema>({
     resolver: zodResolver(createNoteSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    },
   });
 
   async function onSubmit(input: CreateNoteSchema) {
-    alert(input);
+    try {
+      const response = await fetch("/api/notes", {
+        method: "POST",
+        body: JSON.stringify(input),
+      });
+
+      if (!response.ok) throw Error("Status code: " + response.status)
+      form.reset();
+      router.refresh()
+      setOpen(false)
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong. Please try again");
+    }
   }
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -23,6 +60,45 @@ const AddNoteDialog = ({ open, setOpen }: AddNoteDialogProps) => {
         <DialogHeader>
           <DialogTitle>Add Note</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <LoadingButton
+                type="submit"
+                loading={form.formState.isSubmitting}
+              >
+                Submit
+              </LoadingButton>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
