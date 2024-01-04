83d7649a668e3daa8bd39e09bbd3fea4fe279196
tanvirgeek <tanvirgeek@gmail.com>
2024-01-04 06:23:50 +0600

    Showing Notes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
 import React from "react";
 import prisma from "@/lib/db";
+import Note from "@/components/Note";
 
 export const metadata: Metadata = {
   title: "Flowbrain - Notes",
@@ -14,7 +15,18 @@ const Notes = async () => {
     where: { userId },
   });
 
-  return <div>{JSON.stringify(allNotes)}</div>;
+  return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+      {allNotes.length === 0 && (
+        <div className="col-span-full text-center">
+          {"You don't have any notes yet. Why don't you create one?"}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Notes;
